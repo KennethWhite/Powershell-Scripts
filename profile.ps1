ac47ac1a811a429fd35812a131d3a64a0020c687
Kenny White <Kennival@hotmail.com>
2022-01-11 08:43:06 -0800

    Update PS profile

--- profile.ps1
+++ profile.ps1
@@ -1,45 +1,62 @@
 #--------------------------Import Modules--------------------------#
-Import-Module Get-Weather
-Import-Module posh-git
-# Import-Module oh-my-posh
+# Import-Module Get-Weather
+# Import-Module posh-git
+Import-Module oh-my-posh
 
 #--------------------------Configuration--------------------------#
 
-Set-PSReadlineKeyHandler -Chord Ctrl+Spacebar -Function MenuComplete
-Set-PSReadlineOption -BellStyle None
-# Set-Theme Paradox
+# Set-PoshPrompt -Theme craver
+Set-PoshPrompt -Theme C:\Users\KennyWhite\Documents\PowerShell\.mycarvertheme.omp.json
 
 #--------------------------Functions--------------------------#
 
 # Launch explorer in path or in current folder default
-function e ([string] $directory = ".") { ii $directory }
+function e ([string] $directory = ".") { Invoke-Item $directory }
 
 # Requests domain registration info on the specified website
-Function WhoIs([string]$website) {
+Function WhoIs([string]$website){
     C:\UserPrograms\SysinternalsSuite\whois.exe $website
 }
 
 # Walks through each child directory and execute the scriptBlock in each
-function Walk-ChildDirectory {
+function Walk-ChildDirectory
+{
     Param
     (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)][ScriptBlock]$Task
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][ScriptBlock]$Task
     )
-    ls -Directory | % {
-        pushd $_
+    Get-ChildItem -Directory | ForEach-Object{
+        Push-Location $_
         & $Task
-        popd
+        Pop-Location
     }
 }
 
-function Dashboard {
+function Get-AllHashesInDirectory($path)
+{
+    Get-ChildItem -Path $path| ForEach-Object{
+        Get-FileHash -Path $_.FullName
+    }
+}
+
+function Compare-StoredHashes
+{
+    Param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline = $true)]$ReferenceObject,
+        [Parameter(Mandatory=$true, ValueFromPipeline = $true)]$DifferenceObject
+    )
+    (Compare-Object -ReferenceObject $ReferenceObject -DifferenceObject $DifferenceObject  -Property hash -PassThru).Path
+}
+
+function Dashboard{
     Get-Weather -City Spokane
 }
 
 function Show-CPU {
     $iteration = 0
     while ($iteration -le 10) {
-        Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average
+        Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select-Object Average
         Start-Sleep -Milliseconds 100   
     }
 }
@@ -61,7 +78,7 @@ function glp {
 
 
 #---------------------------- NETWORKING ---------------------------------#
-function Display-Network-Devices($Subnet, $FileOut, [bool]$GridView ) {
+function Get-Network-Devices($Subnet, $FileOut, [bool]$GridView ) {
     ## Ping subnet
     if (!($Subnet)) {
         $Subnet = "10.10.42."
@@ -72,7 +89,7 @@ function Display-Network-Devices($Subnet, $FileOut, [bool]$GridView ) {
     }
     $Computers = (arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +', ',' |
         ConvertFrom-Csv -Header Computername, IPv4, MAC, x, Vendor |
-        Select Computername, IPv4, MAC
+        Select-Object Computername, IPv4, MAC
 
     ForEach ($Computer in $Computers) {
         nslookup $Computer.IPv4 | Select-String -Pattern "^Name:\s+([^\.]+).*$" |
@@ -113,24 +130,14 @@ function Get-Applications-With-Active-Internet-Connections {
 Set-Alias walk Walk-ChildDirectory
 $ACE = "C:\Users\KennyWhite\source\repos\-iNPUT"
 
-
 #--------------------------Posh git setup--------------------------#
-$GitPromptSettings.EnableWindowTitle = '~'
-$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
-$GitPromptSettings.DefaultPromptSuffix = "$(">" * ($nestedPromptLevel + 1)) "
-#$GitPromptSettings.AfterText += "`n $([DateTime]::now.ToString("MM-dd HH:mm:ss"))"
-$GitPromptSettings.AfterForegroundColor = "Blue"
-$GitPromptSettings.BeforeForegroundColor = "Blue"
-$GitPromptSettings.DefaultForegroundColor = "Green"
-
-
-
-
-
-
-# $ThemeSettings.GitSymbols.BranchUntrackedSymbol = [char]::ConvertFromUtf32(0xf070)
-# $ThemeSettings.GitSymbols.BranchSymbol = [char]::ConvertFromUtf32(0xe725)
-# $ThemeSettings.GitSymbols.BranchIdenticalStatusToSymbol = [char]::ConvertFromUtf32(0xf0c9)
+# $GitPromptSettings.EnableWindowTitle = '~'
+# $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+# $GitPromptSettings.DefaultPromptSuffix = "$(">" * ($nestedPromptLevel + 1)) "
+# #$GitPromptSettings.AfterText += "`n $([DateTime]::now.ToString("MM-dd HH:mm:ss"))"
+# $GitPromptSettings.AfterForegroundColor = "Blue"
+# $GitPromptSettings.BeforeForegroundColor = "Blue"
+# $GitPromptSettings.DefaultForegroundColor = "Green"
 
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
@@ -138,36 +145,36 @@ function Test-Administrator {
 }
 
 
-function prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
+# function prompt {
+#     $realLASTEXITCODE = $LASTEXITCODE
 
-    Write-Host
+#     Write-Host
 
-    if (Test-Administrator) {
-        # Use different username if elevated
-        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
-    }
+#     if (Test-Administrator) {
+#         # Use different username if elevated
+#         Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+#     }
 
-    if ($s -ne $null) {
-        # color for PSSessions
-        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
-        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
-        Write-Host ") " -NoNewline -ForegroundColor DarkGray
-    }
+#     if ($s -ne $null) {
+#         # color for PSSessions
+#         Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+#         Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+#         Write-Host ") " -NoNewline -ForegroundColor DarkGray
+#     }
 
-    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\', '\\'), "~") -NoNewline -ForegroundColor Blue
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
-    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor Magenta
-    Write-Host " :" -NoNewline -ForegroundColor DarkGray
+#     Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\', '\\'), "~") -NoNewline -ForegroundColor Blue
+#     Write-Host " : " -NoNewline -ForegroundColor DarkGray
+#     Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor Magenta
+#     Write-Host " :" -NoNewline -ForegroundColor DarkGray
 
-    $global:LASTEXITCODE = $realLASTEXITCODE
+#     $global:LASTEXITCODE = $realLASTEXITCODE
 
-    Write-VcsStatus
+#     Write-VcsStatus
 
-    Write-Host ""
+#     Write-Host ""
 
-    return "> "
-}
+#     return "> "
+# }
 
 
 
